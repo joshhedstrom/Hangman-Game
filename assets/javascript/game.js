338f7b2407bec77b3dd8d5419e7e91900f503f65
Josh Hedstrom <josh.hedstrom@gmail.com>
2018-02-25 19:28:42 -0600

    made operational

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,27 +10,47 @@
 
     document.getElementById("start").addEventListener("click", function() {
 
+                if (lettersLeft > 0) {
+                    let guess = prompt("pick a letter, any letter. or cancel to quit")
+                    if (guess === null) {
+                        // break;
+                    } else if (guess.length !== 1) {
+                        alert("one letter at a time, please.")
 
-        console.log(lettersLeft);
-
-        while (lettersLeft > 0) {
-            document.getElementById("word").innerHTML = answerArray.join(" ");
-            let guess = prompt("pick a letter, any letter. or cancel to end")
-            if (guess === null) {
-                break;
-            } else if (guess.length !== 1) {
-                alert("one letter at a time, please.")
-
-            } else {
-                for (let j = 0; j < randomWord.length; j++) {
-                    if (randomWord[j] === guess) {
-                        answerArray[j] = guess
-                        lettersLeft--;
+                    } else {
+                        for (let j = 0; j < randomWord.length; j++) {
+                            if (randomWord[j] === guess) {
+                                answerArray[j] = guess
+                                lettersLeft--;
+                                document.getElementById("word").innerHTML = answerArray.join(" ");
+                                document.getElementById("lettersLeft").innerHTML = lettersLeft;
+                            }
+                        }
                     }
+
                 }
-            }
 
-            break;
+                document.getElementById("guessAgain").addEventListener("click", function() {
+                    if (lettersLeft > 0) {
+                        let guess = prompt("pick a letter, any letter. or cancel to quit")
+                        if (guess === null) {
+                            // break;
+                        } else if (guess.length !== 1) {
+                            alert("one letter at a time, please.")
 
-        }
-    });
+                        } else {
+                            for (let j = 0; j < randomWord.length; j++) {
+                                if (randomWord[j] === guess) {
+                                    answerArray[j] = guess
+                                    lettersLeft--;
+                                    document.getElementById("word").innerHTML = answerArray.join(" ");
+                                    document.getElementById("lettersLeft").innerHTML = lettersLeft;
+                                }
+                            }
+                        }
+
+                    }
+
+                    // alert("nice! the word was: " + randomWord)
+                });
+            });
