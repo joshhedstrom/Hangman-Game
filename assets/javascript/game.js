ac40a552fa10aee578dbd443002590634415bc9f
Josh Hedstrom <josh.hedstrom@gmail.com>
2018-02-26 17:56:42 -0600

    finished as game loop function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,81 +10,36 @@
     console.log(randomWord)
 
 
-function gameLoop() {
-    document.getElementById("word").innerHTML = answerArray.join(" ");
-    if (lettersLeft > 0) {
-        let guess = prompt("pick a letter, any letter")
-        usedArray.push(guess)
-        document.getElementById("lettersUsed").innerHTML = usedArray;
-        if (guess.length !== 1) {
-            alert("one letter at a time, please.")
+    function gameLoop() {
+        document.getElementById("word").innerHTML = answerArray.join(" ");
+        if (lettersLeft > 0) {
+            let guess = prompt("pick a letter, any letter")
+            usedArray.push(guess)
+            document.getElementById("lettersUsed").innerHTML = usedArray;
+            if (guess.length !== 1) {
+                alert("one letter at a time, please.")
 
-        } else {
-            for (let j = 0; j < randomWord.length; j++) {
-                if (randomWord[j] === guess) {
-                    answerArray[j] = guess
-                    lettersLeft--;
-                    document.getElementById("word").innerHTML = answerArray.join(" ");
-                    document.getElementById("lettersLeft").innerHTML = lettersLeft;
+            } else {
+                for (let j = 0; j < randomWord.length; j++) {
+                    if (randomWord[j] === guess) {
+                        answerArray[j] = guess
+                        lettersLeft--;
+                        document.getElementById("word").innerHTML = answerArray.join(" ");
+                        document.getElementById("lettersLeft").innerHTML = lettersLeft;
+                    }
                 }
             }
-        }
 
+        }
+        if (lettersLeft === 0) {
+            let restart = confirm("nice! the word was: " + randomWord + ".\n play again?")
+            if (restart === true) {
+                location.reload(true)
+            } else {
+                alert("thanks for playing!")
+            }
+
+        }
     }
-}
     document.getElementById("start").onclick = gameLoop;
     document.getElementById("guessAgain").onclick = gameLoop;
-
-    //     document.getElementById("word").innerHTML = answerArray.join(" ");
-    //     if (lettersLeft > 0) {
-    //         let guess = prompt("pick a letter, any letter")
-    //         usedArray.push(guess)
-    //         document.getElementById("lettersUsed").innerHTML = usedArray;
-    //         if (guess.length !== 1) {
-    //             alert("one letter at a time, please.")
-
-    //         } else {
-    //             for (let j = 0; j < randomWord.length; j++) {
-    //                 if (randomWord[j] === guess) {
-    //                     answerArray[j] = guess
-    //                     lettersLeft--;
-    //                     document.getElementById("word").innerHTML = answerArray.join(" ");
-    //                     document.getElementById("lettersLeft").innerHTML = lettersLeft;
-    //                 }
-    //             }
-    //         }
-
-    //     }
-
-    //     document.getElementById("guessAgain").addEventListener("click", function() {
-    //         if (lettersLeft > 0) {
-    //             let guess = prompt("pick a letter, any letter. or cancel to quit")
-    //             usedArray.push(guess)
-    //             document.getElementById("lettersUsed").innerHTML = usedArray;
-    //             if (guess.length !== 1) {
-    //                 alert("one letter at a time, please.")
-
-    //             } else {
-    //                 for (let j = 0; j < randomWord.length; j++) {
-    //                     if (randomWord[j] === guess) {
-    //                         answerArray[j] = guess
-    //                         lettersLeft--;
-    //                         document.getElementById("word").innerHTML = answerArray.join(" ");
-    //                         document.getElementById("lettersLeft").innerHTML = lettersLeft;
-    //                     }
-    //                 }
-    //             }
-
-    //         }
-
-    // if (lettersLeft === 0) {
-    // 	let restart = confirm("nice! the word was: " + randomWord + ".\n play again?")
-    // 	 if (restart === true) {
-    // 	 	// statement
-    // 	 } else {
-    // 	 	alert("thanks for playing!")
-    // 	 }
-
-    // }
-    //     });
-
