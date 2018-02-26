6359602acc3a96cc2190e14bef5032e759fd06e0
Josh Hedstrom <josh.hedstrom@gmail.com>
2018-02-25 20:41:33 -0600

    added letters used

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,52 +5,54 @@
     for (let i = 0; i < randomWord.length; i++) {
         answerArray[i] = " _ ";
     }
+    let usedArray = [];
 
     console.log(randomWord)
 
     document.getElementById("start").addEventListener("click", function() {
 
-                if (lettersLeft > 0) {
-                    let guess = prompt("pick a letter, any letter. or cancel to quit")
-                    if (guess === null) {
-                        // break;
-                    } else if (guess.length !== 1) {
-                        alert("one letter at a time, please.")
+        document.getElementById("word").innerHTML = answerArray.join(" ");
+        if (lettersLeft > 0) {
+            let guess = prompt("pick a letter, any letter")
+            usedArray.push(guess)
+            document.getElementById("lettersUsed").innerHTML = usedArray;
+            if (guess.length !== 1) {
+                alert("one letter at a time, please.")
 
-                    } else {
-                        for (let j = 0; j < randomWord.length; j++) {
-                            if (randomWord[j] === guess) {
-                                answerArray[j] = guess
-                                lettersLeft--;
-                                document.getElementById("word").innerHTML = answerArray.join(" ");
-                                document.getElementById("lettersLeft").innerHTML = lettersLeft;
-                            }
+            } else {
+                for (let j = 0; j < randomWord.length; j++) {
+                    if (randomWord[j] === guess) {
+                        answerArray[j] = guess
+                        lettersLeft--;
+                        document.getElementById("word").innerHTML = answerArray.join(" ");
+                        document.getElementById("lettersLeft").innerHTML = lettersLeft;
+                    }
+                }
+            }
+
+        }
+
+        document.getElementById("guessAgain").addEventListener("click", function() {
+            if (lettersLeft > 0) {
+                let guess = prompt("pick a letter, any letter. or cancel to quit")
+                usedArray.push(guess)
+                document.getElementById("lettersUsed").innerHTML = usedArray;
+                if (guess.length !== 1) {
+                    alert("one letter at a time, please.")
+
+                } else {
+                    for (let j = 0; j < randomWord.length; j++) {
+                        if (randomWord[j] === guess) {
+                            answerArray[j] = guess
+                            lettersLeft--;
+                            document.getElementById("word").innerHTML = answerArray.join(" ");
+                            document.getElementById("lettersLeft").innerHTML = lettersLeft;
                         }
                     }
-
                 }
 
-                document.getElementById("guessAgain").addEventListener("click", function() {
-                    if (lettersLeft > 0) {
-                        let guess = prompt("pick a letter, any letter. or cancel to quit")
-                        if (guess === null) {
-                            // break;
-                        } else if (guess.length !== 1) {
-                            alert("one letter at a time, please.")
+            }
 
-                        } else {
-                            for (let j = 0; j < randomWord.length; j++) {
-                                if (randomWord[j] === guess) {
-                                    answerArray[j] = guess
-                                    lettersLeft--;
-                                    document.getElementById("word").innerHTML = answerArray.join(" ");
-                                    document.getElementById("lettersLeft").innerHTML = lettersLeft;
-                                }
-                            }
-                        }
-
-                    }
-
-                    // alert("nice! the word was: " + randomWord)
-                });
-            });
+            // alert("nice! the word was: " + randomWord)
+        });
+    });
